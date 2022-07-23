e150ecb72033622de9656cc835a18aa20c29d1a7
wes-santos <weslleyalmerindo2@gmail.com>
2022-07-23 20:41:58 -0300

    wip

--- src/api.js
+++ src/api.js
@@ -4,10 +4,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200
-}));
+app.use(cors());
 
 app.use(require('./router'));
 
--- src/database/config/config.js
+++ src/database/config/config.js
@@ -5,10 +5,8 @@ const options = {
   password: process.env.MYSQL_PASSWORD || 'root',
   database: process.env.MYSQL_DB_NAME ||'desafio_xp_backend',
   host: process.env.HOSTNAME || '127.0.0.1',
-  dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
+  port: process.env.PORT || '3001',
+  dialect: 'postgres',
 };
 
 module.exports = {
